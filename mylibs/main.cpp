f2342de6e5b4e1b4236f1cb9bd3c445f20ad1160
Simi <siminischi1234@gmail.com>
2021-11-24 22:45:38 +0200

    Add project files.

---
+++ mylibs/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include "matrix.h"
+
+using namespace std;
+
+int main()
+{
+    matrix<int> gay(2, 2);
+}
---
+++ mylibs/matrix.h
@@ -0,0 +1,142 @@
+#pragma once
+#include <iostream>
+
+template <typename T> class matrix
+{
+public:
+	int col, row;
+	T** data;
+
+public:
+	matrix() { col = row = 0; data = nullptr; }	// Wanted to declare outside but doesn't want to work for some odd reason
+	matrix(int column, int row);
+	matrix(const matrix& _array);					// Copy constructor
+	~matrix();
+
+	matrix operator = (const matrix& right);
+	matrix operator += (const matrix& right);
+	matrix operator -= (const matrix& right);
+	matrix operator *= (const matrix& right);
+	matrix operator + (const matrix& right);
+	matrix operator - (const matrix& right);
+	matrix operator * (const matrix& right);
+};
+
+// Beginning of declaration of constructors
+template <typename T> matrix<T> :: matrix(int _col, int _row)
+{
+	// Encapsulation of col and row
+	col = _col;
+	row = _row;
+
+	// Memory allocation for array
+	data = (T**) new T*[col];
+	for (size_t i = 0; i < col; i++)
+		data[i] = (T*) new T[row];
+
+	// Fill array with 0
+	for (size_t i = 0; i < col; i++)
+		for (size_t j = 0; j < row; j++)
+			data[i][j] = 0;
+}
+
+template <typename T> matrix<T> ::matrix(const matrix& _array)
+{
+	col = _array.col;
+	row = _array.row;
+
+	// Not making a function for this, sometimes copy-paste is fine
+	data = (T**) new T * [col];
+	for (size_t i = 0; i < col; i++)
+		data[i] = (T*) new T[row];
+
+	// Copy values over
+	for (size_t i = 0; i < col; i++)
+		for (size_t j = 0; j < row; j++)
+			data[i][j] = _array.data[i][j];
+}
+
+template <typename T> matrix<T> :: ~matrix()
+{
+	if (row)
+		for (size_t i = 0; i < col; i++)
+			delete[] data[i];
+	if (col)
+		delete[] data;
+}
+// End of declaration of constructors
+
+// Beginning of declaration of operator overloading
+template <typename T> matrix<T> matrix<T> :: operator = (const matrix& right)
+{
+	// Free memory
+	if (row)
+		for (size_t i = 0; i < col; i++)
+			delete[] data[i];
+	if (col)
+		delete[] data;
+
+	// Transfer data
+	col = right.col;
+	row = right.row;
+
+	// Reallocate memory
+	data = (T**) new T * [col];
+	for (size_t i = 0; i < col; i++)
+		data[i] = (T*) new T[row];
+
+	// Copy values over
+	for (size_t i = 0; i < col; i++)
+		for (size_t j = 0; j < row; j++)
+			data[i][j] = right.data[i][j];
+	return *this;
+}
+
+template <typename T> matrix<T> matrix<T> :: operator += (const matrix& right)
+{
+	for (size_t i = 0; i < col; i++)
+		for (size_t j = 0; j < row; j++)
+			data[i][j] += right.data[i][j];
+	return *this;
+}
+
+template <typename T> matrix<T> matrix<T> :: operator -= (const matrix& right)
+{
+	for (size_t i = 0; i < col; i++)
+		for (size_t j = 0; j < row; j++)
+			data[i][j] -= right.data[i][j];
+	return *this;
+}
+
+template <typename T> matrix<T> matrix<T> :: operator *= (const matrix& right)
+{
+	for (size_t i = 0; i < col; i++)
+		for (size_t j = 0; j < row; j++)
+			data[i][j] *= right.data[i][j];
+	return *this;
+}
+
+template <typename T> matrix<T> matrix<T> :: operator + (const matrix& right)
+{
+	for (size_t i = 0; i < col; i++)
+		for (size_t j = 0; j < row; j++)
+			data[i][j] += right.data[i][j];
+	return *this;
+}
+
+template <typename T> matrix<T> matrix<T> :: operator - (const matrix& right)
+{
+	for (size_t i = 0; i < col; i++)
+		for (size_t j = 0; j < row; j++)
+			data[i][j] -= right.data[i][j];
+	return *this;
+}
+
+template <typename T> matrix<T> matrix<T> :: operator * (const matrix& right)
+{
+	for (size_t i = 0; i < col; i++)
+		for (size_t j = 0; j < row; j++)
+			data[i][j] *= right.data[i][j];
+	return *this;
+}
+// End of declaration of operator overloading
