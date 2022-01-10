920a4e7ddf850de19ba83d5dcf70b604a33afa01
EpicShadowXD <33294226+EpicShadowXD@users.noreply.github.com>
2022-01-10 02:07:12 +0200

    Changed tests

--- mylibs/main.cpp
+++ mylibs/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include "matrix.cpp"
 
 using namespace std;
@@ -15,17 +14,19 @@ void output(matrix<int>::iterator& it, int n)
 
 int main()
 {
-    matrix<int> myMatrix(3, 3);
+    matrix<int> myMatrix(3, 3), copy(3, 4);
+    matrix<int>::iterator it = nullptr;
 
     myMatrix[0][0] = 1; myMatrix[0][1] = 2; myMatrix[0][2] = 7;
     myMatrix[1][0] = 3; myMatrix[1][1] = 4; myMatrix[1][2] = 8;
     myMatrix[2][0] = 5; myMatrix[2][1] = 6; myMatrix[2][2] = 9;
-    
-    matrix<int>::iterator it = nullptr;
 
-    for (int i = 0; i < myMatrix.col_size(); ++i)
-    {
-        it = myMatrix.row_begin(i);
-        output(it, myMatrix.row_size());
-    }
+    copy = copy + myMatrix;
+    std::cout << copy << std::endl;
+
+    it = copy.colBegin(0);
+    output(it, copy.getColSize());
+
+    it = copy.rowBegin(0);
+    output(it, copy.getRowSize());
 }
