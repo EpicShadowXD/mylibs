01d74346644416045e829ecdb0a5f3a76f72ae5e
EpicShadowXD <33294226+EpicShadowXD@users.noreply.github.com>
2022-01-10 02:04:56 +0200

    Column iterator added, modified col/row methods
    Modified operator overload + and - so they use += and -= instead of copy pasting code.
    Implemented vector of references for column iterator through casting sorcery.
    Added todo algebra prototypes.
    Now have getRowSize and getColSize which now returns the row size and column size (no longer have rowSize which returned numberOfRows meaning what was supposed to be colsize and viceversa).

--- mylibs/matrix.h
+++ mylibs/matrix.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <iostream>
 
-using ll = unsigned long long;	// Hotfix for potential loss of data warning
+// Hotfix for potential loss of data warning
+using ll = unsigned long long;
 
 // TO-DO: create refference wrapper class
 // TO-DO: create size method/ subclass to differentiate between "user used" size and "memory allocated" size
@@ -11,7 +12,7 @@ public:
 	class iterator
 	{
 	public:
-		iterator() : pointer(nullptr), stride(NULL), data(nullptr), columnReferences(new Type& [numberOfRows]) {}
+		iterator() : pointer(nullptr) {}
 		iterator(Type* ptr);
 
 		Type& operator * ();
@@ -33,13 +34,12 @@ public:
 		bool operator != (const iterator&);
 
 	protected:
-		ll stride;
 		Type* pointer;
 	};
 
 public:
-	matrix() : numberOfColumns(NULL), numberOfRows(NULL), data(nullptr) {}
-	matrix(ll, ll);
+	matrix() :	numberOfColumns(NULL), numberOfRows(NULL), data(nullptr), columnVector(nullptr) {}
+	matrix(const ll, const ll);
 	matrix(const matrix&);					// Copy constructor
 	~matrix();
 
@@ -47,43 +47,57 @@ public:
 	matrix operator = (const matrix&);
 
 	// Hides data subcategory and can access data directly through instance
-	Type*& operator [] (ll value);
+	Type*& operator [] (const ll value);
 
 	// Arithmetic
 	// Note: does operations relative to the implied matrix's dimensions, meaning
 	//		that the elements from a bigger matrix outside the scope of the implied
-	//		matrix do not get touched (potential silent runtime error).
+	//		matrix do not get touched (potential silent runtime error) (perfectly 
+	//		fine for linear albegra though).
 	matrix operator += (const matrix&);
 	matrix operator -= (const matrix&);
 
-	matrix operator + (const matrix&);
-	matrix operator - (const matrix&);
+	matrix operator + (matrix&);
+	matrix operator - (matrix&);
 
+	// Algebra - TODO
+	matrix operator + (const double scalar);
+	matrix operator - (const double scalar);
+	matrix operator * (const double scalar);
+
+	matrix operator += (const double scalar);
+	matrix operator -= (const double scalar);
+	matrix operator *= (const double scalar);
+
+	// Iostream output
 	template <typename Type> friend std::ostream& operator << (std::ostream&, const matrix<Type>&);
 	template <typename Type> friend std::istream& operator >> (std::istream&, matrix<Type>&);
 
 	// TO-DO!
 	// Memory management
 	void resize(const ll numberOfColumns, const ll numberOfRows);	// Keeps memory, just destroys elements
-	void shrink_to_fit();									// Frees up unused memory
+	void resize(const ll numberOfColumns, const ll numberOfRows, double value);	// Initialize potential new empty spaces with value
+	void shrink_to_fit();											// Frees up unused memory defined as all 0s on a row or column
 	void erase(const ll colIndex, const ll rowIndex);
 	void erase(const ll firstColIndex, const ll firstRowIndex,
 		const ll secondColIndex, const ll secondRowIndex);
-	void insert(const ll colIndex, const ll rowIndex);	// Requires automatic reallocation of memory; also don'Type forget to implement bounds check
-	void at(const ll colIndex, const ll rowIndex);	// Again, don'Type forget to implement bounds check
+	void insert(const ll colIndex, const ll rowIndex);				// Requires automatic reallocation of memory; also don'Type forget to implement bounds check
+	void at(const ll colIndex, const ll rowIndex);					// Again, don't forget to implement bounds check
 	void reserve(ll n);
 	void swap(matrix& right);
-	void append(matrix& right);	// Concatenate two matrixes
 
-	// Methods for iterators | Too lazy to implement exception handling rn
-	const ll row_size();
-	const ll col_size();
+	// Methods for iterators
+	const ll getRowSize();
+	const ll getColSize();
 
-	Type* row_begin(const ll index);
-	Type* row_end(const ll index);
+	Type* rowBegin(const ll index);
+	Type* rowEnd(const ll index);
+
+	Type* colBegin(const ll index);
+	Type* colEnd(const ll index);
 
 protected:
 	ll numberOfColumns, numberOfRows;
 	Type** data;
-	Type* columnReferences;
+	Type* columnVector;
 };
