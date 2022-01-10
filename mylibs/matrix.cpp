3e08e0a00e7ac0240489c67597b7cfbb851df635
EpicShadowXD <33294226+EpicShadowXD@users.noreply.github.com>
2022-01-10 15:33:47 +0200

    Update matrix.cpp

--- mylibs/matrix.cpp
+++ mylibs/matrix.cpp
@@ -7,79 +7,40 @@ template <typename Type> matrix<Type> ::matrix(const ll _numberOfColumns, const 
 	numberOfColumns = _numberOfColumns;
 	numberOfRows = _numberOfRows;
 
-	// Memory allocation for array
-	data = (Type**) new Type * [numberOfColumns];
-	for (size_t i = 0; i < numberOfColumns; i++)
-		data[i] = (Type*) new Type[numberOfRows];
-
-	// Fill array with 0
-	for (size_t i = 0; i < numberOfColumns; i++)
-		for (size_t j = 0; j < numberOfRows; j++)
-			data[i][j] = 0;
+	allocate(*this, numberOfColumns, numberOfRows);
+	fill(*this, 0);
 
 	columnVector = new Type[numberOfRows];
 	for (size_t i = 0; i < numberOfRows; i++)
 		columnVector[i] = 0;
 }
 
-template <typename Type> matrix<Type> ::matrix(const matrix& _matrix)
+template <typename Type> matrix<Type> ::matrix(const matrix& other)
 {
-	numberOfColumns = _matrix.numberOfColumns;
-	numberOfRows = _matrix.numberOfRows;
+	numberOfColumns = other.numberOfColumns;
+	numberOfRows = other.numberOfRows;
 
-	// Not making a function for this, sometimes copy-paste is fine
-	data = (Type**) new Type * [numberOfColumns];
-	for (size_t i = 0; i < numberOfColumns; i++)
-		data[i] = (Type*) new Type[numberOfRows];
+	allocate(*this, numberOfColumns, numberOfRows);
 
-	// Copy values over
+	// Can't use copy method for this for some odd reason
 	for (size_t i = 0; i < numberOfColumns; i++)
 		for (size_t j = 0; j < numberOfRows; j++)
-			data[i][j] = _matrix.data[i][j];
+			data[i][j] = other.data[i][j];
 
 	columnVector = new Type[numberOfRows];
 	for (size_t i = 0; i < numberOfRows; i++)
-		columnVector[i] = _matrix.columnVector[i];
+		columnVector[i] = other.columnVector[i];
 }
 
-template <typename Type> matrix<Type> :: ~matrix()
-{
-	if (numberOfRows)
-	{
-		for (size_t i = 0; i < numberOfColumns; i++)
-			delete[] data[i];
-		delete[] columnVector;
-	}
-	if (numberOfColumns)
-		delete[] data;
-}
+template <typename Type> matrix<Type> :: ~matrix() { deallocate(*this, this->numberOfColumns, this->numberOfRows); }
 // </constructor>
 
 // <operator overload>
 template <typename Type> matrix<Type> matrix<Type> :: operator = (const matrix& other)
 {
-	if (numberOfRows)
-	{
-		for (size_t i = 0; i < numberOfColumns; i++)
-			delete[] data[i];
-		delete[] columnVector;
-	}
-	if (numberOfColumns)
-		delete[] data;
-
-	// Transfer data
-	numberOfColumns = other.numberOfColumns;
-	numberOfRows = other.numberOfRows;
-
-	// Reallocate memory
-	data = (Type**) new Type * [numberOfColumns];
-	for (size_t i = 0; i < numberOfColumns; i++)
-		data[i] = (Type*) new Type[numberOfRows];
-
-	// Copy values over
-	for (size_t i = 0; i < numberOfColumns; i++)
-		for (size_t j = 0; j < numberOfRows; j++)
-			data[i][j] = other.data[i][j];
+	deallocate(*this, this->numberOfColumns, this->numberOfRows);
+	allocate(*this, numberOfColumns, numberOfRows);
+	copy(*this, other, numberOfColumns, numberOfRows);
 
 	columnVector = new Type[numberOfRows];
 	for (size_t i = 0; i < numberOfRows; i++)
@@ -88,6 +49,7 @@ template <typename Type> matrix<Type> matrix<Type> :: operator = (const matrix& 
 	return *this;
 }
 
+// Matrices
 template <typename Type> matrix<Type> matrix<Type> :: operator += (const matrix& right)
 {
 	for (size_t i = 0; i < std::min(numberOfColumns, right.numberOfColumns); i++)
@@ -107,6 +69,47 @@ template <typename Type> matrix<Type> matrix<Type> :: operator -= (const matrix&
 template <typename Type> matrix<Type> matrix<Type> :: operator + (matrix& right) { *this += right; return *this; }
 
 template <typename Type> matrix<Type> matrix<Type> :: operator - (matrix& right) { *this -= right; return *this; }
+
+// Scalars
+template <typename Type> matrix<Type> matrix<Type> :: operator += (const Type scalar)
+{
+	for (size_t i = 0; i < numberOfColumns; i++)
+		for (size_t j = 0; j < numberOfRows; j++)
+			data[i][j] += scalar;
+	return *this;
+}
+
+template <typename Type> matrix<Type> matrix<Type> :: operator -= (const Type scalar)
+{
+	for (size_t i = 0; i < numberOfColumns; i++)
+		for (size_t j = 0; j < numberOfRows; j++)
+			data[i][j] -= scalar;
+	return *this;
+}
+
+template <typename Type> matrix<Type> matrix<Type> :: operator *= (const Type scalar)
+{
+	for (size_t i = 0; i < numberOfColumns; i++)
+		for (size_t j = 0; j < numberOfRows; j++)
+			data[i][j] *= scalar;
+	return *this;
+}
+
+template <typename Type> matrix<Type> matrix<Type> :: operator /= (const Type scalar)
+{
+	for (size_t i = 0; i < numberOfColumns; i++)
+		for (size_t j = 0; j < numberOfRows; j++)
+			data[i][j] /= scalar;
+	return *this;
+}
+
+template <typename Type> matrix<Type> matrix<Type> :: operator + (const Type scalar) { return *this += scalar; }
+
+template <typename Type> matrix<Type> matrix<Type> :: operator - (const Type scalar) { return *this -= scalar; }
+
+template <typename Type> matrix<Type> matrix<Type> :: operator * (const Type scalar) { return *this *= scalar; }
+
+template <typename Type> matrix<Type> matrix<Type> :: operator / (const Type scalar) { return *this /= scalar; }
 
 template <typename Type> Type*& matrix<Type> :: operator [] (const ll index) { return data[index]; }
 
@@ -130,8 +133,61 @@ template <typename Type> std::istream& operator >> (std::istream& in, matrix<Typ
 // </operator overload>
 
 // <methods>
-template <typename Type> const ll matrix<Type> ::getRowSize() { return numberOfRows; }
-template <typename Type> const ll matrix<Type> ::getColSize() { return numberOfColumns; }
+template <typename Type> void matrix<Type> ::allocate(matrix<Type>& other, ll numberOfColumns, ll numberOfRows)
+{
+	other.data = (Type**) new Type * [numberOfColumns];
+	for (size_t i = 0; i < numberOfColumns; i++)
+		other.data[i] = (Type*) new Type[numberOfRows];
+}
+
+template <typename Type> void matrix<Type> ::deallocate(matrix<Type>& other, ll numberOfColumns, ll numberOfRows)
+{
+	if (numberOfRows)
+	{
+		for (size_t i = 0; i < numberOfColumns; i++)
+			delete[] other.data[i];
+		delete[] other.columnVector;
+	}
+	if (numberOfColumns)
+		delete[] other.data;
+}
+
+// Does not work when right is a reference and can't make
+// right a reference, have no idea why
+template <typename Type> void matrix<Type> ::copy(matrix<Type>& left, const matrix<Type> right, ll numberOfColumns, ll numberOfRows)
+{
+	for (size_t i = 0; i < numberOfColumns; i++)
+		for (size_t j = 0; j < numberOfRows; j++)
+			left.data[i][j] = right.data[i][j];
+}
+
+template <typename Type> void matrix<Type> ::fill(matrix<Type>& other, Type value)
+{
+	// Fill array with value
+	for (size_t i = 0; i < other.numberOfColumns; i++)
+		for (size_t j = 0; j < other.numberOfRows; j++)
+			other.data[i][j] = value;
+}
+
+
+template <typename Type> void matrix<Type> ::resize(const ll newNumberOfColumns, const ll newNumberOfRows)
+{
+	matrix<Type> myCopy(newNumberOfColumns, newNumberOfRows);
+
+	for (size_t i = 0; i < newNumberOfColumns; i++)
+		for (size_t j = 0; j < newNumberOfRows; j++)
+			myCopy.data[i][j] = data[i][j];
+
+	deallocate(*this, this->numberOfColumns, this->numberOfRows);
+	allocate(*this, newNumberOfColumns, newNumberOfRows);
+
+	*this = myCopy;
+	numberOfColumns = newNumberOfColumns;
+	numberOfRows = newNumberOfRows;
+}
+
+template <typename Type> const ll matrix<Type> ::getRowSize() { return numberOfRows; }		// row length = how many columns there are
+template <typename Type> const ll matrix<Type> ::getColSize() { return numberOfColumns; }	// and viceversa
 
 template <typename Type> Type* matrix<Type> ::rowBegin(const ll index) { return data[index]; }
 template <typename Type> Type* matrix<Type> ::rowEnd(const ll index) { return data[index] + numberOfColumns; }
@@ -139,7 +195,7 @@ template <typename Type> Type* matrix<Type> ::rowEnd(const ll index) { return da
 template <typename Type> Type* matrix<Type> ::colBegin(const ll index)
 {
 	for (size_t i = 0; i < numberOfColumns; i++)
-		columnVector[i] = (Type&)data[i][index];	// I have no idea why this works
+		columnVector[i] = (Type&)data[i][index];	// I have no idea why this works | Edit: does nothing
 	return columnVector;
 }
 
@@ -148,14 +204,6 @@ template <typename Type> Type* matrix<Type> ::colEnd(const ll index)
 	for (size_t i = 0; i < numberOfColumns; i++)
 		columnVector[i] = (Type&)data[i][index];
 	return columnVector + numberOfColumns;
-}
-
-template <typename Type> void matrix<Type> ::resize(const ll newColSize, const ll newRowSize)
-{
-	// Pass over numberOfColumns and numberOfRows
-	numberOfColumns = newColSize;
-	numberOfRows = newRowSize;
-	return;
 }
 // </methods>
 
