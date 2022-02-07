0657e38d2f4628172db351fd683680ffe054929d
EpicShadowXD <33294226+EpicShadowXD@users.noreply.github.com>
2022-02-07 15:05:26 +0200

    Update main.cpp

--- mylibs/main.cpp
+++ mylibs/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void outputUp(matrix<int>::iterator& it, int n)
+void outputUp(matrix<int>::iterator it, int n)
 {
     for (int i = 0; i < n; ++i)
     {
@@ -12,7 +12,7 @@ void outputUp(matrix<int>::iterator& it, int n)
     } std::cout << std::endl;
 }
 
-void outputDown(matrix<int>::iterator& it, int n)
+void outputDown(matrix<int>::iterator it, int n)
 {
     for (int i = 0; i < n; ++i)
     {
@@ -24,29 +24,35 @@ void outputDown(matrix<int>::iterator& it, int n)
 int main()
 {
     // matrix<Type> name(numberOfColumns, numberOfRows)
-    matrix<int> myMatrix(3, 3), copy(3, 3);
+    matrix<int> myMatrix(3, 3);
     matrix<int>::iterator it = nullptr;
 
     myMatrix[0][0] = 1; myMatrix[0][1] = 2; myMatrix[0][2] = 7;
     myMatrix[1][0] = 3; myMatrix[1][1] = 4; myMatrix[1][2] = 8;
     myMatrix[2][0] = 5; myMatrix[2][1] = 6; myMatrix[2][2] = 9;
 
-    copy = myMatrix;
-    std::cout << copy << std::endl;
+    matrix<int> copy(myMatrix);
+
+    //std::cout << copy << std::endl;
     // Error at resize
-    //copy.resize(2, 2);
+    //copy.resize(10, 10);
+    //std::cout << copy[5][5];
 
-    it = copy.colBegin(0);
-    std::cout << "Forwards\n";
-    outputUp(it, copy.getColSize());
+    //it = copy.colBegin(1);
 
-    // Silent runtime error
-    // columnIterator has old values, not references
-    copy[0][0] = 10;
-    copy[1][0] = 11;
-    copy[2][0] = 12;
+    //std::cout << **(copy.columnVector + 0) << ' ' << **(copy.columnVector + 1) << ' ' << **(copy.columnVector + 2) << '\n';
+    //std::cout << *copy.columnVector[0] << ' ' << *copy.columnVector[1] << ' ' << *copy.columnVector[2] << '\n';
 
-    it--;
-    std::cout << "Backwards\n";
-    outputDown(it, copy.getColSize());
+    //outputUp(it, 3);
+
+    //copy[0][0] = 10;
+    //copy[1][0] = 11;
+    //copy[2][0] = 12;
+
+    //std::cout << copy << std::endl;
+
+    //outputUp(it, 3);
+
+    //std::cout << **(copy.columnVector + 2) << ' ' << **(copy.columnVector + 1) << ' ' << **(copy.columnVector + 0) << '\n';
+    //std::cout << *copy.columnVector[2] << ' ' << *copy.columnVector[1] << ' ' << *copy.columnVector[0] << '\n';
 }
