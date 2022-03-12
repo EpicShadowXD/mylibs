73d0eb96f3ff2ce399cf0128fef2f1e709afafcd
Siminischi A Sebastian <33294226+EpicShadowXD@users.noreply.github.com>
2022-03-13 00:07:50 +0200

    Update matrix.h

--- mylibs/matrix.h
+++ mylibs/matrix.h
@@ -2,65 +2,21 @@
 #include <iostream>
 #include <stdexcept>
 
-// Will never use negative numbers for matrix size
-using ll = unsigned long long;
-
 // TODO: create size method/ subclass to differentiate 
 // between "user used" size and "memory allocated" size
 template <typename Type> class matrix
 {
-// Iterator
-public:
-	class iterator
-	{
-	protected:
-		Type* pointer;
-
-	public:
-		iterator() : pointer(nullptr) {}
-		iterator(Type* ptr);
-
-		Type& operator * ();
-		//Type&& operator * ();
-
-		Type* operator = (const iterator&);
-		Type* operator = (Type**);
-
-		// Prefix
-		Type& operator + (Type);
-		Type& operator - (Type);
-
-		Type& operator ++ ();
-		Type& operator -- ();
-
-		// Postfix
-		iterator operator ++ (Type);
-		iterator operator -- (Type);
-
-		Type& operator += (const Type);
-		Type& operator -= (const Type);
-
-		bool operator == (const iterator&);
-		bool operator != (const iterator&);
-	};
-
-// Matrix
 private:
-	ll numberOfColumns, numberOfRows;
+	size_t rowSize, colSize;
 	Type** data;
 
 public:
-	Type** columnVector;
+	using colIterator = Type*;
 
-public:
-
-	matrix() :	numberOfColumns(NULL), numberOfRows(NULL), data(nullptr), columnVector(nullptr) {}
-	matrix(const ll, const ll);
+	matrix() :	rowSize(NULL), colSize(NULL), data(nullptr) {}
+	matrix(const size_t, const size_t);
+	matrix(const size_t, const size_t, Type);
 	matrix(const matrix& other);
-	//matrix(const matrix& other) :	numberOfColumns { other.numberOfColumns },
-									//numberOfRows { other.numberOfRows }
-									//{ allocate(*this, numberOfColumns, numberOfRows);
-									//copy(*this, other, numberOfColumns, numberOfRows); } // Copy constructor
 	
 	~matrix();
 
@@ -68,13 +24,9 @@ public:
 	matrix operator = (const matrix&);
 
 	// Hides data subcategory and can access data directly through instance
-	Type*& operator [] (const ll value);
+	Type*& operator [] (const size_t value);
 
 	// Arithmetic
-	// Note: does operations relative to the implied matrix's dimensions, meaning
-	//		that the elements from a bigger matrix outside the scope of the implied
-	//		matrix do not get touched (potential silent runtime error) (perfectly 
-	//		fine for linear albegra though).
 	matrix operator += (const matrix&);
 	matrix operator -= (const matrix&);
 
@@ -91,44 +43,56 @@ public:
 	matrix operator *= (const Type);
 	matrix operator /= (const Type);
 
+	// TODO
+	// Algebra
+	double determinant();
+
+	// Iostream operator overload
 	template <typename Type> friend std::ostream& operator << (std::ostream&, const matrix<Type>&);
 	template <typename Type> friend std::istream& operator >> (std::istream&, matrix<Type>&);
 
 	// TODO
+	// Fstream operator overload
+
+	// TODO
 	// Memory management
-	void fill(matrix&, const Type);
-	void resize(const ll numberOfColumns, const ll numberOfRows);	// Keeps memory, just destroys elements
-	void resize(const ll numberOfColumns, const ll numberOfRows, const Type value);	// Initialize potential new empty spaces with value
+	void resize(const size_t rowSize, const size_t colSize);	// Keeps memory, just destroys elements
+	void resize(const size_t rowSize, const size_t colSize, const Type value);	// Initialize potential new empty spaces with value
 	void shrink_to_fit();											// Frees up unused memory defined as all 0s on a row or column
-	void erase(const ll colIndex, const ll rowIndex);
-	void erase(const ll firstColIndex, const ll firstRowIndex,
-		const ll secondColIndex, const ll secondRowIndex);
-	void insert(const ll colIndex, const ll rowIndex);				// Requires automatic reallocation of memory; also don'Type forget to implement bounds check
-	void at(const ll colIndex, const ll rowIndex);					// Again, don't forget to implement bounds check
-	void reserve(const ll);
+	void erase(const size_t colIndex, const size_t rowIndex);
+	void erase(const size_t firstColIndex, const size_t firstRowIndex,
+		const size_t secondColIndex, const size_t secondRowIndex);
+	void insert(const size_t colIndex, const size_t rowIndex);				// Requires automatic reallocation of memory; also don'Type forget to implement bounds check
+	void at(const size_t colIndex, const size_t rowIndex);					// Again, don't forget to implement bounds check
+	void reserve(const size_t);
 	void swap(matrix& right);
+	void fill(Type);
 
 	// Methods for iterators
-	const ll getRowSize();
-	const ll getColSize();
+	const size_t getRowSize();
+	const size_t getColSize();
 
 	const Type* matrixBegin();
 	const Type* matrixEnd();
 
-	Type* rowBegin(const ll);
-	Type* rowEnd(const ll);
+	Type* rowBegin(const size_t);
+	Type* rowEnd(const size_t);
 
-	Type** colBegin(const ll);
-	Type** colEnd(const ll);
+	matrix::colIterator colBegin(const size_t);
+	matrix::colIterator colEnd(const size_t);
 
 private:
 	// Currently used
-	Type** allocateMatrix(ll, ll);
-	Type** allocateColumnVector(ll);
+	Type** allocateMatrix(const size_t, const size_t);
+	Type** allocateColumnVector(const size_t);
 
-	void deallocateMatrix(matrix&, ll, ll);
+	void deallocateMatrix(matrix&, size_t, size_t);
 
-	// Should be deprecated
-	void allocate(matrix&, ll, ll);
-	void deallocate(matrix&, ll, ll);
+	// Distinction between public fill and private fill for distinct
+	// purpose outside class and ease of use for code inside the class.
+	void fill(matrix&, const Type);
+
+	// Consider it deprecated, might remove it later.
+	void allocate(matrix&, size_t, size_t);
+	void deallocate(matrix&, size_t, size_t);
 };
